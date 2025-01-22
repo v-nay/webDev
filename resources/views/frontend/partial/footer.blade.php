4c8d3b21646dbfb87da085d91ca597a041b3883f
vinay.maharjan <vinay.maharjan@ekbana.info>
2025-01-22 12:01:03 +1100

    footer changes

--- resources/views/frontend/partial/footer.blade.php
+++ resources/views/frontend/partial/footer.blade.php
@@ -7,10 +7,10 @@
       <!-- Social Media Links -->
       <nav class="footer-right">
         <span>Follow us:</span>
-        <a href="{{ getCmsConfig('facebook')}}"><img src="images/facebook-icon.png" alt="Facebook"></a>
-        <a href="{{getCmsConfig('instagram')}}"><img src="images/insta-icon.png" alt="Instagram"></a>
-        <a href="{{getCmsConfig('x')}}"><img src="images/X-icon.png" alt="Twitter"></a>
-        <a href="{{getCmsConfig('youtube')}}"><img src="images/yt-icon.png" alt="YouTube"></a>
+        <a href="{{ 'https://'.getCmsConfig('facebook')}}"><img src="images/facebook-icon.png" alt="Facebook"></a>
+        <a href="{{'https://'.getCmsConfig('instagram')}}"><img src="images/insta-icon.png" alt="Instagram"></a>
+        <a href="{{'https://'.getCmsConfig('x')}}"><img src="images/X-icon.png" alt="Twitter"></a>
+        <a href="{{'https://'.getCmsConfig('youtube')}}"><img src="images/yt-icon.png" alt="YouTube"></a>
       </nav>
     </div>
   </footer>
